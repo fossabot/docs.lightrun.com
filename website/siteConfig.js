8c8a2366fbd4d4e1c45704eb143a0ccbc7715a73
Rachel Cheyfitz - Owner <spinrach@gmail.com>
2020-11-13 13:56:08 +0200

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Lightrun Documentation', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://spinningrachel.github.io/docs.lightrun.com/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/index.html', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
