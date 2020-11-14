a5073592314bc7eaaded223ae83e1b026d546c34
ANIRBAN DUTTA <anirbandutta011@gmail.com>
2020-11-14 19:43:48 +0530

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -25,7 +25,7 @@ const siteConfig = {
   tagline: 'The shift-left observability debugging tool made for developers',
   url: "https://spinningrachel.github.io/docs.lightrun.com/" /* your website url */,
   editUrl: "https://github.com/spinningrachel/docs.lightrun.com",
-  baseUrl: "/docs.lightrun.com/" /* base url for your project */,
+  baseUrl: "/" /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
